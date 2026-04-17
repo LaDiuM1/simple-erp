c53e3ae6e5254510cfc4a7c2cc45ae6d1c7a7c6b
LaDiuM <wjddhrtjd123@gmail.com>
2026-04-17 20:33:12 +0900

    feat(be): BaseEntity 및 JPA 상태 감지 기능 추가
    - 엔티티별 생성, 수정 상태 관리를 위한 BaseEntity.java 추가
    - JPA 쿼리 시 변경 자동 감지를 위한 JpaConfig.java 추가

---
+++ backend/src/main/java/io/github/ladium1/erp/global/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package io.github.ladium1.erp.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+// JPA 변경 감지 설정 =
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
